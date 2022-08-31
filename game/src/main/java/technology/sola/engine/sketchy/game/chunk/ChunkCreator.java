250663429a857ac55c7022d914575337b8080ae1
iamdudeman <timothy.solum@gmail.com>
2022-08-31 00:32:13 -0500

    Initial functionality of cliff shaping (top and top-left only) working. Made todo for joining with nearby cliffs

--- game/src/main/java/technology/sola/engine/sketchy/game/chunk/ChunkCreator.java
+++ game/src/main/java/technology/sola/engine/sketchy/game/chunk/ChunkCreator.java
@@ -8,6 +8,8 @@ import static technology.sola.engine.sketchy.game.chunk.Chunk.TILE_SIZE;
 
 public class ChunkCreator {
   private static final Random RANDOM = new Random();
+  private static final int cultureGenerations = 5;
+  private static final int cliffBlocksToJoin = 2;
 
   public Chunk createChunk(ChunkId chunkId, Vector2D playerTranslate) {
     TileComponent[][] tileComponents = new TileComponent[Chunk.COLUMNS][Chunk.ROWS];
@@ -45,7 +47,7 @@ public class ChunkCreator {
   }
 
   private void stepCulture(TileComponent[][] tileComponents, int depth) {
-    if (depth > 3) {
+    if (depth > cultureGenerations - 1) {
       return;
     }
 
@@ -54,7 +56,6 @@ public class ChunkCreator {
         TileComponent tileComponent = tileComponents[column][row];
         TileType tileType = tileComponent.getTileType();
 
-        // cliff first
         if (tileType == TileType.CLIFF_CENTER) {
           float value = RANDOM.nextFloat();
 
@@ -65,7 +66,6 @@ public class ChunkCreator {
           }
         }
 
-        // dirt second
         if (tileType == TileType.DIRT) {
           float value = RANDOM.nextFloat();
 
@@ -82,11 +82,27 @@ public class ChunkCreator {
   }
 
   private void stepShapeCliffs(TileComponent[][] tileComponents) {
-    // todo
+    for (int row = 0; row < Chunk.ROWS; row++) {
+      for (int column = 0; column < Chunk.COLUMNS; column++) {
+        TileComponent tileComponent = tileComponents[column][row];
+
+        if (tileComponent.getTileType() == TileType.CLIFF_CENTER) {
+          joinNearbyCliffs(tileComponents, column, row);
+
+          if (!shapeCliff(tileComponents, row, column)) {
+            tileComponent.setTileType(TileType.GRASS);
+          }
+        }
+      }
+    }
   }
 
   private void stepPlacePickups(TileComponent[][] tileComponents) {
-    // todo
+    for (int row = 0; row < Chunk.ROWS; row++) {
+      for (int column = 0; column < Chunk.COLUMNS; column++) {
+        // todo
+      }
+    }
   }
 
   private void stepCleanup(TileComponent[][] tileComponents) {
@@ -98,15 +114,64 @@ public class ChunkCreator {
     int rowOffset = direction == 0 ? -1 : direction == 1 ? 1 : 0; // 0 -> -1, 1 -> 1, else 0
     int columnOffset = direction == 2 ? -1 : direction == 3 ? 1 : 0; // 2 -> -1, 3 -> 1, else 0
 
+    TileComponent tileComponent = peak(tileComponents, startRow + rowOffset, startColumn + columnOffset);
 
-    if (startColumn + columnOffset < 0 || startColumn + columnOffset >= tileComponents.length) {
-      return;
+    if (tileComponent != null) {
+      tileComponent.setTileType(tileType);
+    }
+  }
+
+  private void joinNearbyCliffs(TileComponent[][] tileComponents, int startRow, int startColumn) {
+    // TODO join if "cliffBlocksToJoin" block away
+  }
+
+  /**
+   *
+   * @param tileComponents
+   * @param startRow
+   * @param startColumn
+   * @return false if cliff cannot be shaped
+   */
+  private boolean shapeCliff(TileComponent[][] tileComponents, int startRow, int startColumn) {
+    TileComponent topTile = peak(tileComponents, startRow - 1, startColumn);
+
+    if (topTile == null) {
+      return false;
     }
 
-    if (startRow + rowOffset < 0 || startRow + rowOffset >= tileComponents[startColumn].length) {
-      return;
+    if (isReplaceable(topTile.getTileType())) {
+      topTile.setTileType(TileType.CLIFF_TOP);
+
+      TileComponent topLeftTile = peak(tileComponents, startRow - 1, startColumn - 1);
+
+      if (topLeftTile == null) {
+        topTile.setTileType(TileType.GRASS);
+        return false;
+      }
+
+      if (isReplaceable(topLeftTile.getTileType())) {
+        topLeftTile.setTileType(TileType.CLIFF_TOP_LEFT);
+      } else if (topLeftTile.getTileType() == TileType.CLIFF_CENTER) {
+        return shapeCliff(tileComponents, startRow - 1, startColumn - 1);
+      }
     }
 
-    tileComponents[startColumn + columnOffset][startRow + rowOffset].setTileType(tileType);
+    return true;
+  }
+
+  private boolean isReplaceable(TileType tileType) {
+    return tileType == TileType.GRASS || tileType == TileType.DIRT;
+  }
+
+  private TileComponent peak(TileComponent[][] tileComponents, int row, int column) {
+    if (column < 0 || column >= tileComponents.length) {
+      return null;
+    }
+
+    if (row < 0 || row >= tileComponents[column].length) {
+      return null;
+    }
+
+    return tileComponents[column][row];
   }
 }
