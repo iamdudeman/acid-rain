0b2226fba16209809d21a00537c51c6e9a0b818c
iamdudeman <timothy.solum@gmail.com>
2022-09-01 10:42:32 -0500

    Fixed checkstyle issues. Cliffs can now spawn a bit closer to player

--- game/src/main/java/technology/sola/engine/sketchy/game/SpriteCache.java
+++ game/src/main/java/technology/sola/engine/sketchy/game/SpriteCache.java
@@ -7,15 +7,15 @@ import java.util.Map;
 
 public final class SpriteCache {
   public static final SpriteKeyFrame ERASED = new SpriteKeyFrame(Constants.Assets.Sprites.SPRITE_SHEET_ID, Constants.Assets.Sprites.ERASED);
-  private static final Map<SpriteCacheKey, SpriteKeyFrame> spriteCache = new HashMap<>();
+  private static final Map<SpriteCacheKey, SpriteKeyFrame> SPRITE_CACHE = new HashMap<>();
 
   public static SpriteKeyFrame get(String assetId, String variation) {
     SpriteCacheKey spriteCacheKey = new SpriteCacheKey(assetId, variation);
-    SpriteKeyFrame spriteKeyFrame = spriteCache.get(spriteCacheKey);
+    SpriteKeyFrame spriteKeyFrame = SPRITE_CACHE.get(spriteCacheKey);
 
     if (spriteKeyFrame == null) {
       spriteKeyFrame = new SpriteKeyFrame(Constants.Assets.Sprites.SPRITE_SHEET_ID, assetId + "-" + variation);
-      spriteCache.put(spriteCacheKey, spriteKeyFrame);
+      SPRITE_CACHE.put(spriteCacheKey, spriteKeyFrame);
     }
 
     return spriteKeyFrame;
