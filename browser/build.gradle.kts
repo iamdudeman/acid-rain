27e7f3a6179a54685a853329c0f6d684b8f8296b
iamdudeman <timothy.solum@gmail.com>
2024-01-31 20:37:02 -0600

    Updated sola game engine version and simplified build scripts

--- browser/build.gradle.kts
+++ browser/build.gradle.kts
@@ -2,37 +2,9 @@ plugins {
   id("sola.java-conventions")
 }
 
+apply(plugin = "technology.sola.plugins.sola-web-distribution")
+
 dependencies {
   implementation("com.github.iamdudeman.sola-game-engine:platform-browser:${project.properties["solaVersion"]}")
   implementation(project(":game"))
-}
-
-task("generateWebHtmlAndJs", type = JavaExec::class) {
-  group = "build"
-
-  dependsOn(tasks.getByPath("assemble"))
-
-  classpath = sourceSets.main.get().runtimeClasspath
-  setArgsString("build ${project.name}-${project.version}.jar")
-  inputs.file("build/libs/${project.name}-${project.version}.jar")
-  outputs.file("build/sola.js")
-  mainClass.set("${project.properties["basePackage"]}.browser.GenerateBrowserFilesMain")
-}
-
-task("distWebZip", Zip::class) {
-  group = "distribution"
-  destinationDirectory.set(file(buildDir))
-  archiveBaseName.set("${project.properties["gameName"]}-${project.name}")
-
-  dependsOn(tasks.getByName("generateWebHtmlAndJs"))
-
-  from("${project.rootDir}/assets") {
-    into("assets")
-  }
-  from("${buildDir}/index.html")
-  from("${buildDir}/sola.js")
-}
-
-tasks.assemble {
-  finalizedBy(tasks.getByName("generateWebHtmlAndJs"))
 }
--- javafx/build.gradle.kts
+++ javafx/build.gradle.kts
@@ -1,86 +1,17 @@
+import technology.sola.plugins.SolaJavaDistributionPluginExtension
+
 plugins {
   id("sola.java-conventions")
 }
 
+apply(plugin = "technology.sola.plugins.sola-java-distribution")
+
 dependencies {
   implementation("com.github.iamdudeman.sola-game-engine:platform-javafx:${project.properties["solaVersion"]}")
   implementation(project(":game"))
-
-  val osClassifier = getOsClassifier()
-
-  runtimeOnly("org.openjfx", "javafx-base", "17.0.2", classifier = osClassifier)
-  runtimeOnly("org.openjfx", "javafx-controls", "17.0.2", classifier = osClassifier)
-  runtimeOnly("org.openjfx", "javafx-graphics", "17.0.2", classifier = osClassifier)
 }
 
-tasks.withType<Zip> {
-  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-}
-
-tasks.withType<Tar> {
-  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-}
-
-task("distFatJar", Jar::class) {
-  group = "distribution"
-  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-
-  val osClassifier = getOsClassifier()
-  archiveBaseName.set("${project.properties["gameName"]}-${project.name}-${osClassifier}")
-
-  manifest {
-    attributes["Main-Class"] = "${project.properties["basePackage"]}.${project.name}.JavaFxMain"
-  }
-
-  val dependencies = configurations.runtimeClasspath.get().map(::zipTree)
-
-  from(dependencies)
-  from("${project.rootDir}/assets") {
-    into("assets")
-  }
-  with(tasks.jar.get())
-  destinationDirectory.set(file("$buildDir/dist"))
-  dependsOn(configurations.runtimeClasspath)
-}
-
-task("prepareJPackage", Delete::class) {
-  delete("$buildDir/jpackage")
-}
-
-task("distWinJPackage", Exec::class) {
-  group = "distribution"
-  dependsOn(tasks.getByName("prepareJPackage"))
-  dependsOn(tasks.getByName("distFatJar"))
-
-  executable("jpackage")
-
-  args(
-    "--name", "${project.properties["gameName"]}-${project.version}",
-    "--app-version", "${project.version}",
-    "--vendor", project.properties["vendor"],
-    "--dest", "$buildDir/jpackage",
-    "--input", "$buildDir/dist",
-    "--main-jar", "${project.properties["gameName"]}-${project.name}-win-${project.version}.jar",
-    "--type", "app-image"
-  )
-}
-
-task("distWinJPackageZip", Zip::class) {
-  group = "distribution"
-  destinationDirectory.set(file("$buildDir/jpackage"))
-  archiveBaseName.set("${project.properties["gameName"]}-win-${project.name}")
-
-  dependsOn(tasks.getByName("distWinJPackage"))
-
-  from("${buildDir}/jpackage/${project.properties["gameName"]}-${project.version}")
-}
-
-fun getOsClassifier(): String {
-  if (org.apache.tools.ant.taskdefs.condition.Os.isFamily(org.apache.tools.ant.taskdefs.condition.Os.FAMILY_MAC)) {
-    return "mac"
-  } else if (org.apache.tools.ant.taskdefs.condition.Os.isFamily(org.apache.tools.ant.taskdefs.condition.Os.FAMILY_UNIX)) {
-    return "linux"
-  }
-
-  return "win"
+configure<SolaJavaDistributionPluginExtension> {
+  mainClass = "${project.properties["basePackage"]}.${project.name}.JavaFxMain"
+  useJavaFx = true
 }
